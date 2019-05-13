1bad4b911381b0c600bb4519da9f1f3dd82fb65c
unknown <victorst79@gmail.com>
2019-05-13 22:26:17 +0200

    add theme in vue and working /w components

--- App/src/main.js
+++ App/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
+Vue.use(VueMaterial);
 Vue.config.productionTip = false
+
+const linkActiveClass = 'my-link-active-class'
+
+// pass custom class to Vue Material
+Vue.material.router.linkActiveClass = linkActiveClass
 
 new Vue({
   router,
